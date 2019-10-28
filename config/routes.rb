598cefde6f5ad6c6b7f7143d8623827027948743
eva bernal <5.22.1@hotmail.ca>
2019-10-28 16:08:24 -0400

    basic model setup: students+notes, indexes viewable

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'students/index'
+  get 'notes/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  resources :students, :notes
 end
