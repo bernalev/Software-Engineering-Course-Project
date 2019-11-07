bebb4b6b8c3d44451424aa2557c99e6ab53f33f1
eva bernal <5.22.1@hotmail.ca>
2019-11-06 19:06:59 -0500

    trying to go back to working on windows

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'students/index'
+  get 'notes/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'welcome/index'
+
   root 'welcome#index'
+
+  resources :students, :notes
 end
