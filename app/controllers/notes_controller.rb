79ebaba4ed5ec654d42801755b0263711425fea5
Timothy Chern <tim.chern@mail.utoronto.ca>
2019-11-21 18:05:05 -0500

    feature to upload non-text files, adding show view for note. Should only work locally.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,6 +6,7 @@ class NotesController < ApplicationController
   def create
   	@course_instance = CourseInstance.find(params[:course_instance_id])
   	@note = @course_instance.notes.new(note_params)
+    @note.file.attach(params[:note][:file])
   	@note.user = User.find_by(id:current_user.id)
   	@note.save
   	redirect_to course_instance_path(@course_instance)
@@ -18,9 +19,14 @@ class NotesController < ApplicationController
   	redirect_to course_instance_path(@course_instance)
   end
 
+  def show
+    @course_instance = CourseInstance.find(params[:course_instance_id])
+    @note = @course_instance.notes.find(params[:id])
+  end
+
 
   private
   	def note_params
-  		params.require(:note).permit(:title, :body)
+  		params.require(:note).permit(:title, :body, :file)
   	end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,5 @@
 class Note < ApplicationRecord
 	belongs_to :user
 	belongs_to :course_instance
+	has_one_attached :file
 end
