24e77e5fef664cadefb16b6daa318bcfe054a27d
vagrant <vagrant@vagrant.vm>
2019-11-22 20:53:12 +0000

    fix broken migration

--- db/migrate/20191122200411_create_tutor_ads.rb
+++ db/migrate/20191122200411_create_tutor_ads.rb
@@ -1,6 +1,6 @@
 class CreateTutorAds < ActiveRecord::Migration[6.0]
   def change
-    create_table :TutorAds to |t|
+    create_table :TutorAds do |t|
       t.string :title ,:type  
       t.text :body 
       t.float :rate 
