a07f1f15b0b375a2b5f1ef8b6636de3a5cdcaece
eva bernal <5.22.1@hotmail.ca>
2019-11-24 12:04:49 -0500

    using faker to generate data for all models

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,13 +3,9 @@ class Course < ApplicationRecord
 	belongs_to :school
 	has_and_belongs_to_many :users
 
-	DEPTS = [
-		"Computer Engineering",
-		"Electrical Engineering",
-		"Mechanical Engineering",
-		"Computer Science",
-		"Arts and Science",
-		"Economics"
-	]
+	DEPTS =  ['Arts', 'Business', 'Education', 'Applied Science (Psychology)', 'Architectural Technology', 
+		'Biological Science', 'Biomedical Science', 'Commerce', 'Communications', 'Creative Arts', 
+		'Criminology', 'Design', 'Engineering', 'Forensic Science', 'Health Science', 'Information Systems', 
+		'Computer Science', 'Law', 'Nursing', 'Medicine', 'Psychology', 'Teaching']
 	validates :department, inclusion: {in: DEPTS}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-num_schools = 15
-#num_users = 15
-#num_courses = 15
+num_schools = 20
+num_users = 15
+num_courses = 30
+num_notes = 120
+num_course_instances = 60
 
 schools_array = Array.new(num_schools)
 for i in 0..schools_array.length
@@ -16,7 +18,6 @@ for i in 0..schools_array.length
 end
 schools = School.create(schools_array)
 
-=begin
 # TODO name should match email, how do
 users_array = Array.new(num_users)
 for i in 0..users_array.length
@@ -28,57 +29,44 @@ for i in 0..users_array.length
 		first_name: Faker::Name.first_name, last_name: Faker::Name.last_name}
 end
 users = User.create(users_array)
-=end
 
-users = User.create([
-	{email: "test@example.com", password: "my password", password_confirmation: "my password",
-		first_name: "Alejandro", last_name: "Smith"},
-	{email: "23@hotmail.ca", password: "password", password_confirmation: "password",
-		first_name: "Eva", last_name: "Bernal"},
-	{email: "Dennis@example.com", password: "123456", password_confirmation: "123456",
-		first_name: "Dennis", last_name: "Schroder"}
-])
 
-=begin 
 # course_code not a given attribute of Faker::Educator
-# maybe get from actual uni site
+# maybe get from actual uni site ?
 courses_array = Array.new(num_courses)
 for i in 0..courses_array.length
-	courses_array[i] = {}
+	courses_array[i] = {
+		course_code: "#{Faker::Alphanumeric.alpha(number: 3)}#{Faker::Number.number(digits: 3)}".upcase, 
+		course_name: Faker::Educator.course_name, 
+		school: schools[Faker::Number.between(from: 0, to: num_schools-1)], 
+		department: Faker::Educator.subject
+	}
 end
-
-courses = School.create(courses_array)
-=end
-
-
-courses = Course.create([
-	{course_code: "ECE568", course_name: "Computer Security", school: schools.first, department: "Computer Engineering"},
-	{course_code: "ECE444", course_name: "Software Eng", school: schools.first, department: "Computer Engineering"},
-	{course_code: "APS112", course_name: "ESP II", school: schools.first, department: "Mechanical Engineering"},
-	{course_code: "GEO108", course_name: "Geographis of Everyday Life", school: schools.second, department: "Arts and Science"},
-	{course_code: "ECON 101", course_name: "Principles of Microeconomics", school: schools.third, department: "Economics"},
-
-])
+courses = Course.create(courses_array)
 
 users.first.courses << courses.first
 users.second.courses << courses.second
 users.second.courses << courses.third
 
-course_instances = CourseInstance.create([
-	{year: 2015, semester: "Winter", professor: "Mark Anthony", course: courses.fourth},
-	{year: 2018, semester: "Summer", professor: "Anderson", course: courses.third},
-	{year: 2012, semester: "Fall", professor: "Bill Nye", course: courses.second},
-	{year: 2015, semester: "Winter", professor: "Garcia", course: courses.first},
-	{year: 2016, semester: "Winter", professor: "Garcia", course: courses.first}
-])
+seasons = ["Winter", "Fall", "Spring", "Summer", "Autumn", "Year"]
+course_instances_array = Array.new(num_course_instances)
+for i in 0..course_instances_array.length
+	course_instances_array[i] = {
+		year: Faker::Number.between(from: 2010, to: 2019), 
+		semester: seasons[Faker::Number.between(from: 0, to: seasons.length-1)], 
+		professor: Faker::Name.name, 
+		course: courses[Faker::Number.between(from: 0, to: num_courses-1)]
+	}
+end
+course_instances = CourseInstance.create(course_instances_array)
 
-Note.create([
-	{title:"my first note", body:"hello world", 
-		user: users.first, course_instance: course_instances.second},
-	{title:"software engineering", body:"agile>waterfall", 
-		user: users.second, course_instance: course_instances.third},
-	{title:"day one", body:"it's dangerous out there", 
-		user: users.third, course_instance: course_instances.fourth},
-	{title:"introduction", body:"be careful", 
-		user: users.first, course_instance: course_instances.fifth}
-])
+
+notes_array = Array.new(num_notes)
+for i in 0..notes_array.length
+	notes_array[i] = 	{
+		title: Faker::Book.title, body: Faker::Lorem.paragraphs, 
+		user: users[Faker::Number.between(from: 0, to: num_users-1)], 
+		course_instance: course_instances[Faker::Number.between(from: 0, to: num_course_instances-1)]
+	}
+end
+notes = Note.create(notes_array)
