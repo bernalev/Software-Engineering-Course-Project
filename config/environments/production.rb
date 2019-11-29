a91d92287be63d8fead3fee373bd1450a8ba44b7
eva bernal <5.22.1@hotmail.ca>
2019-11-29 18:18:34 -0500

    add prod config vars for mail service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mailgun.org',
+    port:                 587,
+    domain:               'study-buddy-2019.herokuapp.com',
+    user_name:            '',
+    password:             '',
+    authentication:       'plain',
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
